af97ab2a9562567812e518fd0dc6fe868f909218
jojomondag <josef.nobach@outlook.com>
2023-10-26 20:21:04 +0200

    Working on summary
    Working  on  summary for the logs.

--- Helpers/MessageHelper.mjs
+++ Helpers/MessageHelper.mjs
@@ -5,40 +5,69 @@ import { promises as fsPromises } from 'fs';
 import { parse } from 'stack-trace';
 
 function extractFilePath(error) {
-  const trace = parse(error);
-  const callSite = trace[1];  // The first call site is usually the location where the error was thrown
-  if (callSite) {
+  if (error instanceof Error && error.stack) {
+    const trace = parse(error);
+    const callSite = trace[1];
+    if (callSite) {
       return {
-          filePath: callSite.getFileName(),
-          lineNumber: callSite.getLineNumber(),
+        filePath: callSite.getFileName(),
+        lineNumber: callSite.getLineNumber(),
       };
+    }
   }
   return { filePath: 'Unknown file', lineNumber: 'Unknown line' };
 }
 
+function generateLogMessage({timestamp, level, message, error}) {
+  const { filePath, lineNumber } = error ? extractFilePath(error) : { filePath: 'Unknown file', lineNumber: 'Unknown line' };
+  return `${timestamp} ${level}: ${message} (File: ${filePath}, Line: ${lineNumber})`;
+}
+
+function formatMessage(message, error) {
+  return `${message} (File: ${extractFilePath(error).filePath}, Line: ${extractFilePath(error).lineNumber})`;
+}
+
+function log(type, message, error = null) {
+  const formattedMessage = formatMessage(message, error);
+  this.logger[type](formattedMessage, { error });
+  this.messages.push(createMessageObject(formattedMessage, type));
+}
+
+function createMessageObject(formattedMessage, type) {
+  return { 
+    [type]: formattedMessage, 
+    timestamp: new Date().toISOString() 
+  };
+}
+
+function colorizeLevel(level) {
+  switch (level) {
+    case 'info': return chalk.green(level);
+    case 'warn': return chalk.yellow(level);
+    case 'error': return chalk.red(level);
+    default: return level;
+  }
+}
+
 function createLogger(logDirectory) {
-  // Ensure the log directory exists
   if (!fs.existsSync(logDirectory)) {
     fs.mkdirSync(logDirectory);
   }
+  
+  const logFormat = winston.format.combine(
+    winston.format.timestamp({format: 'h:mm:ss A'}),
+    winston.format.printf(info => generateLogMessage(info))
+  );
 
   return winston.createLogger({
     level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.printf(info => {
-        const { timestamp, level, message, error } = info;
-        const { filePath, lineNumber } = error ? extractFilePath(error) : { filePath: 'Unknown file', lineNumber: 'Unknown line' };
-        return `${timestamp} ${level}: ${message} (File: ${filePath}, Line: ${lineNumber})`;
-      })
-    ),
+    format: logFormat,
     transports: [
       new winston.transports.File({ filename: `${logDirectory}/error.json`, level: 'error' }),
       new winston.transports.File({ filename: `${logDirectory}/combined.json` })
     ]
   });
 }
-
 class MessageHelper {
   constructor() {
     this.logDirectory = 'logs';
@@ -49,45 +78,39 @@ class MessageHelper {
 
   addConsoleTransport() {
     if (process.env.NODE_ENV !== 'production') {
+      const consoleFormat = winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.printf(info => `${chalk.blue(info.timestamp)} ${colorizeLevel(info.level)}: ${info.message}`)
+      );
+      
       this.logger.add(new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.timestamp(),
-          winston.format.printf(info => {
-            const { timestamp, level, message } = info;
-            return `${chalk.blue(timestamp)} ${colorizeLevel(level)}: ${message}`;
-          })
-        )
+        format: consoleFormat
       }));
     }
   }
 
   logSuccess(message, error = null) {
-    const formattedMessage = formatMessage(message, error);
-    this.logger.info(formattedMessage, { error });
-    const successMessage = createErrorMessage(formattedMessage);
-    this.messages.push(successMessage);
-}
+    log.call(this, 'info', message, error);
+  }
 
   logError(message, error) {
-    const formattedMessage = formatMessage(message, error);
-    this.logger.error(formattedMessage, { error });
-    const errorMessage = createErrorMessage(formattedMessage);
-    this.messages.push(errorMessage);
-}
+    log.call(this, 'error', message, error);
+  }
 
-
-logWarn(message, error = null) {
-  const formattedMessage = formatMessage(message, error);
-  this.logger.warn(formattedMessage, { error });
-  const warnMessage = createErrorMessage(formattedMessage);
-  this.messages.push(warnMessage);
-}
+  logWarn(message, error = null) {
+    log.call(this, 'warn', message, error);
+  }
 
   async saveMessagesToJsonFile() {
     const logFilePath = `${this.logDirectory}/ExecutionLog.json`;
+    const currentDate = new Date();
 
     const aggregatedMessages = {
+      Summary: {
+        Date: currentDate.toISOString().split('T')[0],
+        Hour: currentDate.toLocaleTimeString()
+      },
       Errors: this.messages.filter(msg => msg.error),
       Successful: this.messages.filter(msg => msg.success)
     };
@@ -104,27 +127,6 @@ logWarn(message, error = null) {
   async flushLogs() {
     await this.saveMessagesToJsonFile();
     this.messages = [];  // Clear the messages queue after saving
-  }
-}
-function formatMessage(message, error) {
-  const { filePath, lineNumber } = error instanceof Error ? extractFilePath(error) : { filePath: 'Unknown file', lineNumber: 'Unknown line' };
-  return `${message} (File: ${filePath}, Line: ${lineNumber})`;
-}
-function createErrorMessage(formattedMessage) {
-  return { error: formattedMessage, timestamp: new Date().toISOString() };
-}
-
-
-function colorizeLevel(level) {
-  switch (level) {
-    case 'info':
-      return chalk.green(level);
-    case 'warn':
-      return chalk.yellow(level);
-    case 'error':
-      return chalk.red(level);
-    default:
-      return level;
   }
 }
 
